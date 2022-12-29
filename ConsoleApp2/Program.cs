9558f6d2b4ff1f7980fedd84f1ad00c3e3f2b962
Bhaktikanchan532 <kanchanbhakti@gmail.com>
2022-12-29 14:57:22 +0530

    Homework1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //atithmatic operators
+            //Console.WriteLine("Enter a numver");
+            //  int a =Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a number");
+            // int b=Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter a number");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //addition
+            // int sum = a + b+c;
+            // Console.WriteLine($"Sum is {sum}"); 
+            //Substraction
+            //int sub = a - b;
+            // Console.WriteLine($"Sub is{sub}");
+            //multiplication
+            //int multiply = a * b;
+            //Console.WriteLine($"Multiplication is {multiply}");
+            //Division here we need type casting so we use double
+            //double div = (double)a / b;
+            // Console.WriteLine($"Division is {div}");
+            //mod
+            // int mod = a % b;
+            // Console.WriteLine($"Mod is {mod}");
+
+
+
+            // assignment operator
+            //int a = 10, b = 10, c = 10, d = 10;
+            // a += 10;
+            // b -= 5;
+            // c *= 5;
+            // d /= 2;
+            // Console.WriteLine($" value of a {a} value of b {b} value of c {c} value of d {d}");
+
+
+            //relational operator
+            ///int a = 10, b = 9;
+            // Console.WriteLine(a<b);
+            //Console.WriteLine(a<=b);
+            //Console.WriteLine(a>b);
+            //Console.WriteLine(a>=b);
+            //Console.WriteLine(a==b);
+            //Console.WriteLine(a!=b);
+
+
+            //accept value of radius fromuser and claculate area of circle
+            // Console.WriteLine("Enter Radius of circle");
+            //int r=Convert.ToInt32(Console.ReadLine());
+            //double A = (double)3.14*r*r;
+            //Console.WriteLine($"Area of circle is{A}");
+
+
+            //accept 3 marks of student from user and calculate total and percentage
+            // Console.WriteLine("Enter sub1");
+            // int a=Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter sub2");
+            // int b=Convert.ToInt32(Console.ReadLine());
+            //  Console.WriteLine("Enter sub3");
+            //int c=Convert.ToInt32(Console.ReadLine());
+            // int total = a + b + c;
+            //  Console.WriteLine($"Total is{total}");
+            //double percentage = (double)total / 3;
+            //  Console.WriteLine($" percentage is {percentage}");
+
+
+            //display name age percentage city gender mobile number
+            //  Console.WriteLine("Enter your Name");
+            // string name = Console.ReadLine();
+            //Console.WriteLine("Enter your Age");
+            //int age=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your Percentage");
+            //double percentage=Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter your City");
+            //string city = Console.ReadLine();
+            //Console.WriteLine("Enter your Gender");
+            //char gender = Convert.ToChar(Console.ReadLine());         
+            //Console.WriteLine($"Name is {name} Age is {age} Percentage is {percentage} City is {city} Gender is {gender} ");
+
+
+            // accept value of radius fromuser and claculate area of reactangle
+            // Console.WriteLine("Enter length of rectangle");
+            // int l=Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter breadth of rectangle");
+            // int b = Convert.ToInt32(Console.ReadLine());
+            //  int A = l*b;
+            //  Console.WriteLine($"Area of rectangle is{A}");
+
+
+            // accept number from user and claculate its squre and cube
+            // Console.WriteLine("Enter your number");
+            // int a = Convert.ToInt32(Console.ReadLine());         
+            // int x = a*a;
+            // int y = a * a * a;
+            // Console.WriteLine($"Squre is{x} cube is {y}");
+
+
+            //accept id name basic salary from user and calculate hra=12% of basic,da=20% of basic salary,ta=10% of basic salary,pf=12% of basic salary,total salary and net paid salary
+            // Console.WriteLine("Enter emp Name");
+            //string name=Console.ReadLine();
+            //Console.WriteLine("Enter emp id");
+            //int id = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter Basic Salary");
+            // int BS=Convert.ToInt32(Console.ReadLine());
+            // double HRA = (double)BS *0.12;
+            // Console.WriteLine($" HRA IS {HRA}");
+            // double DA = (double)BS *0.20;
+            //Console.WriteLine($" DA IS {DA}");
+            //double TA = (double)BS *0.10;
+            // Console.WriteLine($" TA IS {TA}");
+            // double PF = (double)BS *0.12;
+            //Console.WriteLine($" PF IS {PF}");
+            // double totalsalary = BS+HRA+TA+DA;
+            // Console.WriteLine($" Totalsalary IS {totalsalary}");
+            // double netpaid = totalsalary-PF;
+            // Console.WriteLine($" netpaid salary IS {netpaid}");
+
+
+            //swapping withoutout 3rd variable
+            // Console.WriteLine("Enter your number");
+            // int x=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your number");
+            //int y =Convert.ToInt32(Console.ReadLine());
+            //x = x + y;
+            //y = x - y;
+            //x = x - y;
+            // Console.WriteLine($" x is {x} y is {y}");
+
+
+            //swapping using 3rd variable
+            //Console.WriteLine("Enter your number");
+            //int x=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter your number");
+            //int y =Convert.ToInt32(Console.ReadLine());
+            //int z = x;
+            // x = y;
+            // y = z ;
+            // Console.WriteLine($" x is {x} y is {y}");
+
+            //accept number from user and check wheather it is even odd using if else
+            //Console.WriteLine("Enter your number");
+            // int n = Convert.ToInt32(Console.ReadLine());
+            // if(n%2==0)
+            // {
+            //     Console.WriteLine("Even Number");
+            //  }
+            // else
+            // {
+            //     Console.WriteLine("Odd Number");
+            // }
+
+
+            //find number is positive negative or zero
+            // Console.WriteLine("Enter any number");
+            // int n = Convert.ToInt32(Console.ReadLine());
+            // if(n==0)
+            //  {
+            //     Console.WriteLine($"{n} is zero");
+            // }
+            //else if(n>0)
+            //{
+            //    Console.WriteLine($"{n} is positive");
+            //}
+            // else
+            //{
+            //    Console.WriteLine($"{n} is negative");
+            //}
+
+
+
+ //accept year from user and print leap year or not
+            //Console.WriteLine("Enter year");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //if(n%4==0)
+            // {
+            //     Console.WriteLine("Leap year");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Not leap year");
+            // }
+
+
+
+ //accept 3sub marks calculate percentage and find grade A B C Fail
+            //Console.WriteLine("Enter Sub1");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(" Enter Sub2");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Enter Sub3");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //int total = a + b + c;
+            //Console.WriteLine($"Total is {total}");
+            //double percentage = (double)total / 3;
+            // Console.WriteLine($"percentage is{percentage}");
+            //if(percentage > 75)
+            //{
+            //    Console.WriteLine($"{percentage} A Grade");
+            //}
+            //else if(percentage>50 && percentage<=75)
+            //    Console.WriteLine($"{percentage} B Grade");
+            //}
+            // else if(percentage>40 && percentage<=50)
+            // {
+            //     Console.WriteLine($"{percentage} C Grade");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"{percentage} Fail");
+            // }
+
+
+
+ //Accept number from user and find gretest from 3
+            //  Console.WriteLine("Enter your number");
+            //  int n1 = Convert.ToInt32(Console.ReadLine());
+            //  int n2 = Convert.ToInt32(Console.ReadLine());
+            // int n3 = Convert.ToInt32(Console.ReadLine());
+            // if (n1 > n2)
+            // {
+            //    if (n1 > n3)
+            //      {
+            //          Console.WriteLine($"{n1} is greater");
+            //      }
+            //     else
+            //     {
+            //        Console.WriteLine($"{n3} is greater");
+            //     }
+            // }
+            // else if (n2 > n3)
+            // {
+            //     Console.WriteLine($"{n2} is Greater");
+            // }
+            // else
+            // {
+            //     Console.WriteLine(($"{n3} is greater");
+            // }
+
+
+ //write a program to accept number and print day 
+           // Console.WriteLine("Enter your numbere");
+           // int n = Convert.ToInt32(Console.ReadLine());
+          //  if(n==0)
+           // {
+           //     Console.WriteLine("MONDAY");
+           // }
+           // else if(n==1)
+          //  {
+          //      Console.WriteLine("TUESDAY");
+          //  }
+           // else if(n==2)
+           // {
+           //     Console.WriteLine("WEDNESDAY");
+           // }
+           // else if(n==3)
+           // {
+           //     Console.WriteLine("THURESDAY");
+           // }
+          //  else if(n==4)
+          //  {
+          //      Console.WriteLine("FRIDAY");
+            //}
+           // else if(n==5)
+            //{
+          //      Console.WriteLine("SATURDAY");
+           // }
+          //  else if(n==6)
+          //  {
+          //      Console.WriteLine("SUNDAY");
+          //  }
+          //  else
+          //  {
+          //      Console.WriteLine("Invalid input");
+            //}
+
+
+
+
+ // calculate profit and loss
+            Console.WriteLine("Enter Actual value of product");
+            int AV = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Selling Value of Product");
+            int SV = Convert.ToInt32(Console.ReadLine());
+            
+            if(SV>AV)
+            {
+                int profit = SV - AV;
+                Console.WriteLine($"{profit} is Profit");
+            }
+            else
+            {
+                int loss = AV - SV;
+                Console.WriteLine($"{loss} is Loss");
+            }
+
+        }
+    }
+}
